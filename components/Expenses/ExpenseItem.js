d2041e1a45016962c761dc70efb75cb802d3cf73
louisql <102247814+louisql@users.noreply.github.com>
2022-11-07 20:42:04 -0500

    managing dynamic array

--- components/Expenses/ExpenseItem.js
+++ components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
@@ -6,23 +6,16 @@ import './ExpenseItem.css';
 
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-
-  const clickHandler = () => {
-    setTitle('Updated!');
-    console.log(title);
-  }
 
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date}/>
       
       <div className='expense-item__description'>
-        <h2>{title}</h2> 
+        <h2>{props.title}</h2> 
         {/* originaly props.title but we wanted to be able to modify it with clickHandler so we stored it in a varaible */}
         <div className='expense-item__price'>${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
